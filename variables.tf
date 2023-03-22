6d75eda225f132a38224b1b11853ecb1d8cce6fc
Tomas Balaz <tomasbalaz3012@gmail.com>
2023-03-22 15:52:58 +0100

    chore: bump default helm chart version to 1.4.8

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "1.4.2"
+  default     = "1.4.8"
   description = "Version of the Helm chart"
 }
 
