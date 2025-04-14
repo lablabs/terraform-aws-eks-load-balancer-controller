e496984c76a8a15d99a26332610302ab1c17cafa
Sass Dávid <51000212+sassdavid@users.noreply.github.com>
2025-04-14 13:35:51 +0200

    feat(iam): update iam policy to be compatible version 2.12.0 of lb-controller (#26)
    * feat(iam): update iam policy to be compatible version 2.12.0 of lb-controller
    
    * keep old webhook policy
    
    * update default helm chart version to 1.12.0
    
    * doc update

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "1.10.1"
+  default     = "1.12.0"
   description = "Version of the Helm chart"
 }
 
