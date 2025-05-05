8d3289ec4117a9b5b31a193d911f8c75f7b67aef
Matej Hasul <matej.hasul@lablabs.io>
2025-05-05 11:26:38 +0200

    feat(addon): add migration resources

--- migrations.tf
+++ migrations.tf
@@ -17,3 +17,29 @@ moved {
   from = helm_release.argo_application[0]
   to   = module.addon.helm_release.argo_application[0]
 }
+
+# pod-identity
+moved {
+  from = aws_eks_pod_identity_association.this[0]
+  to   = aws_eks_pod_identity_association.pod_identity[0]
+}
+
+moved {
+  from = aws_iam_policy.eks_pod_identity[0]
+  to   = aws_iam_policy.pod_identity[0]
+}
+
+moved {
+  from = aws_iam_policy.eks_pod_identity[0]
+  to   = aws_iam_policy.pod_identity[0]
+}
+
+moved {
+  from = aws_iam_role.eks_pod_identity[0]
+  to   = aws_iam_role.pod_identity[0]
+}
+
+moved {
+  from = aws_iam_role_policy_attachment.eks_pod_identity[0]
+  to   = aws_iam_role_policy_attachment.pod_identity[0]
+}
