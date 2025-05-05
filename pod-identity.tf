f24fa26152893dda88556f0af181ddf14b46a5dd
Matej Hasul <matej.hasul@lablabs.io>
2025-05-05 07:12:34 +0200

    refactor(pod-identity): prefix local vars in pod-identity.tf

--- pod-identity.tf
+++ pod-identity.tf
@@ -1,11 +1,11 @@
 locals {
-  service_account_create    = var.service_account_create != null ? var.service_account_create : true
-  service_account_name      = var.service_account_name != null ? var.service_account_name : try(local.addon.name)
-  service_account_namespace = var.service_account_namespace != null ? var.service_account_namespace : local.addon_namespace
-  rbac_create               = var.rbac_create != null ? var.rbac_create : true
-  pod_identity_role_create  = var.enabled && local.rbac_create && local.service_account_create && var.pod_identity_role_create
-  pod_identity_role_name    = var.pod_identity_role_name != null ? var.pod_identity_role_name : trim("${var.pod_identity_role_name_prefix}-${local.addon.name}", "-")
-  pod_identity_policy       = var.pod_identity_policy != null ? var.pod_identity_policy : data.aws_iam_policy_document.this[0].json
+  pod_identity_service_account_create    = var.service_account_create != null ? var.service_account_create : true
+  pod_identity_service_account_name      = var.service_account_name != null ? var.service_account_name : try(local.addon.name)
+  pod_identity_service_account_namespace = var.service_account_namespace != null ? var.service_account_namespace : local.addon_namespace
+  rbac_create                            = var.rbac_create != null ? var.rbac_create : true
+  pod_identity_role_create               = var.enabled && local.rbac_create && local.pod_identity_service_account_create && var.pod_identity_role_create
+  pod_identity_role_name                 = var.pod_identity_role_name != null ? var.pod_identity_role_name : trim("${var.pod_identity_role_name_prefix}-${local.addon.name}", "-")
+  pod_identity_policy                    = var.pod_identity_policy != null ? var.pod_identity_policy : data.aws_iam_policy_document.this[0].json
 }
 
 data "aws_iam_policy_document" "pod_identity" {
@@ -63,8 +63,8 @@ resource "aws_eks_pod_identity_association" "pod_identity" {
   count = local.pod_identity_role_create ? 1 : 0
 
   cluster_name    = var.cluster_name
-  namespace       = local.service_account_namespace
-  service_account = local.service_account_name
+  namespace       = local.pod_identity_service_account_namespace
+  service_account = local.pod_identity_service_account_name
   role_arn        = aws_iam_role.pod_identity[0].arn
   tags            = var.pod_identity_tags
 }
