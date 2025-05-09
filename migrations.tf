bc8d7054a9fc90328aae7c851d6684627fdefc37
Matej Hasul <matej.hasul@lablabs.io>
2025-05-09 13:34:21 +0200

    feat(addon): add moved blocks to ease migration

--- migrations.tf
+++ migrations.tf
@@ -1,45 +1,42 @@
-moved {
-  from = aws_iam_policy.this[0]
-  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_policy.this[0]
-}
-
-moved {
-  from = aws_iam_role.this[0]
-  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_role.this[0]
-}
-
-moved {
-  from = aws_iam_role_policy_attachment.this[0]
-  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_role_policy_attachment.this[0]
-}
-
+# Argo
 moved {
   from = helm_release.argo_application[0]
   to   = module.addon.helm_release.argo_application[0]
 }
 
-# pod-identity
+# IRSA
+moved {
+  from = aws_iam_policy.this[0]
+  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_policy.irsa[0]
+}
+
+moved {
+  from = aws_iam_role.this[0]
+  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_role.irsa[0]
+}
+
+moved {
+  from = aws_iam_role_policy_attachment.this[0]
+  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_role_policy_attachment.irsa[0]
+}
+
+# Pod identity
 moved {
   from = aws_eks_pod_identity_association.this[0]
-  to   = aws_eks_pod_identity_association.pod_identity[0]
+  to   = module.addon-irsa["aws-load-balancer-controller"].aws_eks_pod_identity_association.pod_identity[0]
 }
 
 moved {
   from = aws_iam_policy.eks_pod_identity[0]
-  to   = aws_iam_policy.pod_identity[0]
-}
-
-moved {
-  from = aws_iam_policy.eks_pod_identity[0]
-  to   = aws_iam_policy.pod_identity[0]
+  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_policy.pod_identity[0]
 }
 
 moved {
   from = aws_iam_role.eks_pod_identity[0]
-  to   = aws_iam_role.pod_identity[0]
+  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_role.pod_identity[0]
 }
 
 moved {
   from = aws_iam_role_policy_attachment.eks_pod_identity[0]
-  to   = aws_iam_role_policy_attachment.pod_identity[0]
+  to   = module.addon-irsa["aws-load-balancer-controller"].aws_iam_role_policy_attachment.pod_identity[0]
 }
