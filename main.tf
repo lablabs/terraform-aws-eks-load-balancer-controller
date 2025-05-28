e498f21930dfc233b887bc3a1ca64fb7010e3a19
Attila Sovak <attila.sovak@gmail.com>
2025-05-28 07:46:05 +0200

    docs: Docs tls disable TLS1.0 and TLS1.1 (#32)
    # Description
    Add info to docs to disable TLS 1.0 and TL1.1
    
    Reffer to the issue: https://github.com/lablabs/tf-infra/issues/902
    
    ## Type of change
    
    - [ ] A bug fix (PR prefix `fix`)
    - [ ] A new feature (PR prefix `feat`)
    - [ ] A code change that neither fixes a bug nor adds a feature (PR
    prefix `refactor`)
    - [ ] Adding missing tests or correcting existing tests (PR prefix
    `test`)
    - [ ] Changes that do not affect the meaning of the code like
    white-spaces, formatting, missing semi-colons, etc. (PR prefix `style`)
    - [ ] Changes to our CI configuration files and scripts (PR prefix `ci`)
    - [X] Documentation only changes (PR prefix `docs`)
    
    ## How Has This Been Tested?
    Visualy

--- main.tf
+++ main.tf
@@ -3,8 +3,19 @@
  *
  * A Terraform module to deploy the [AWS Load Balancer Controller](https://github.com/kubernetes-sigs/aws-load-balancer-controller) on Amazon EKS cluster.
  *
- * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-load-balancer-controller/actions/workflows/validate.yaml)
- * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-load-balancer-controller/actions/workflows/pre-commit.yml)
+ * > [!CAUTION]
+ * > **Security:** Disable TLS 1.0 and TLS 1.1
+ * >
+ * > We strongly recommend avoiding the use of TLS 1.0 and TLS 1.1, as they contain critical vulnerabilities.
+ * > If you are configuring an Ingress or a Service of type LoadBalancer on AWS, ensure that you select a secure TLS/SSL policy by adding the appropriate annotations. This will help enforce the use of modern, secure protocols.
+ * > Example annotations:
+ * > - Ingress: `alb.ingress.kubernetes.io/ssl-policy: LBSecurityPolicy-TLS13-1-2-2021-06`
+ * > - Service: `service.beta.kubernetes.io/aws-load-balancer-ssl-negotiation-policy: LBSecurityPolicy-TLS13-1-2-2021-06 `
+ * >
+ * > Reffer to the [AWS Annotations Guide](https://kubernetes-sigs.github.io/aws-load-balancer-controller/latest/guide/ingress/annotations/) and [AWS ALB Guide](https://docs.aws.amazon.com/elasticloadbalancing/latest/application/describe-ssl-policies.html) to choose the best option for your deployment
+ *
+ * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-load-balancer-controller/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-load-balancer-controller/actions/workflows/validate.yaml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-load-balancer-controller/actions/workflows/pre-commit.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-load-balancer-controller/actions/workflows/pre-commit.yml)
  */
 locals {
   addon = {
