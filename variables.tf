1cc183f7421ecbb962d1f983aab80106df08a07b
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-12-15 22:09:10 +0100

    chore: apply sync patch

--- variables.tf
+++ variables.tf
@@ -1,10 +1,4 @@
 # IMPORTANT: Add addon specific variables here
-variable "enabled" {
-  type        = bool
-  default     = true
-  description = "Set to false to prevent the module from creating any resources."
-}
-
 variable "aws_partition" {
   type        = string
   default     = "aws"
